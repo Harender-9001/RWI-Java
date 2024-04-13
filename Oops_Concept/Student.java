539ac66d09be5b8b6516d53aae3b88ee7016e65a
Harrybhai1 <harenderrathorepalwal@gmail.com>
2024-04-13 15:46:25 +0530

    successfully implements the concept of object, class, access modifier, getter() and setter() and this keyword

--- Oops_Concept/Student.java
+++ Oops_Concept/Student.java
@@ -1,14 +1,14 @@
 package Oops_Concept;
 // it is good practice to use the getter and setter instead of using public modifier
 public class Student {
-    private String name = "Harender";     //private access specifier/modifier
+    private String name = "Harender";   //private access specifier/modifier
     int rollNumber = 9001;
 
-    public String getName() {   // getter() to get private value
+    public String getName() {    // getter() to get private value
         return name;
     }
 
-    public void setName(String name) {    // setter() to set the value of private variable
-        this.name = name;    // here this keyword is the reference of current object
+    public void setName(String name) {   // setter() to set the value of private variable
+        this.name = name;   // here this keyword is the reference of current object
     }
 }
--- Oops_Concept/StudentUse.java
+++ Oops_Concept/StudentUse.java
@@ -4,8 +4,8 @@ public class StudentUse {
     public static void main(String[] args) {
         Student s1;    // create the reference s1 in stack
         s1 = new Student();  // create object with all properties in heap
-        Student s2 = new Student();  // create reference s2 in stack that reference to the object with all properties in heap
-        System.out.println(s2);      // print memory address of s1
+        Student s2 = new Student();   // create reference s2 in stack that reference to the object with all properties in heap
+        System.out.println(s2);       // print memory address of s1
         System.out.println(s1.getName() + " " + s1.rollNumber);
         s2.setName("Kirti");  // set value to private variable
         s2.rollNumber = 9007;
