2ba9e97166b7d55acd2c3fbe8990e9783777b248
Harrybhai1 <harenderrathorepalwal@gmail.com>
2024-04-13 16:36:11 +0530

    successfully implements the concept constructor

---
+++ Oops_Concept/StudentUse2.java
@@ -0,0 +1,12 @@
+package Oops_Concept;
+
+public class StudentUse2 {
+    public static void main(String[] args) {
+        Student s1 = new Student();
+        Student s2 = new Student("Kirti", 9007);
+        Student s3 = new Student("Aakash");
+        s1.print();
+        s2.print();
+        s3.print();
+    }
+}
