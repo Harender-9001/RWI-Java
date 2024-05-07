069180d3142bfae07b270baec93d8a51f5ee597d
Harrybhai1 <harenderrathorepalwal@gmail.com>
2024-05-07 16:44:07 +0530

    Get even odd number using lambda expression

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,4 @@
-import work.lambda.SumInter;
+import work.lambda.*;
 
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
@@ -6,37 +6,51 @@ public class Main {
     public static void main(String[] args) {
         // Press Alt+Enter with your caret at the highlighted text to see how
         // IntelliJ IDEA suggests fixing it.
-        System.out.println("Hello and welcome!");
+//        System.out.println("Hello and welcome!");
 
-//        MyInter obj = new MyInterImple();
-//        obj.say();
-//
-        //using our interface with help of anonymous class
-//        MyInter obj1 = new MyInter() {
-//            @Override
-//            public void say() {
-//                System.out.println("My first anonymous class");
-//            }
-//        };
-//        obj1.say();
-//
-//        MyInter obj2 = new MyInter() {
-//            @Override
-//            public void say() {
-//                System.out.println("My second anonymous class");
-//            }
-//        };
-//        obj2.say();
+        MyInter obj = new MyInterImple();
+        obj.say();
 
-        //using our interface with help of lambda
-//        MyInter obj3 = () -> System.out.println("this is lambda ");
-//        obj3.say();
+//        using our interface with help of anonymous class
+        MyInter obj1 = new MyInter() {
+            @Override
+            public void say() {
+                System.out.println("My first anonymous class");
+            }
+        };
+        obj1.say();
 
+        MyInter obj2 = new MyInter() {
+            @Override
+            public void say() {
+                System.out.println("My second anonymous class");
+            }
+        };
+        obj2.say();
+
+//        using our interface with help of lambda
+        MyInter obj3 = () -> System.out.println("this is lambda ");
+        obj3.say();
+
+        //Addition using lambda
         SumInter sumInter = (a, b) -> a + b;
-
         int res = sumInter.sum(4,6);
         System.out.println("The sum is :" + res);
         res = sumInter.sum(12,65);
         System.out.println("The new sum is :" + res);
+
+        //get length of given string using lambda
+        LenInter lenInter = str -> str.length();
+        System.out.println("The length of given string is :" + lenInter.getLength("Hello my name is Harender"));
+
+        //check if a given number is even or odd using lambda
+        EvenOddInter evenOddInter = num -> {
+            if(num%2 == 0){
+                System.out.println("The given number " + num + "is a even number");
+            }else {
+                System.out.println("The given number " + num + " is a odd number");
+            }
+        };
+        evenOddInter.getEvenOdd(345);
     }
 }
