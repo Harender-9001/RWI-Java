c7eb56b3a83641cb38d243ee8c543a6fd0835f62
Harrybhai1 <harenderrathorepalwal@gmail.com>
2024-05-12 22:33:12 +0530

    implements method reference

---
+++ Java_8_Features/MethodRef/MethodRefDemo.java
@@ -0,0 +1,33 @@
+package Java_8_Features.MethodRef;
+
+class Stuff {
+    public void say(){
+        System.out.println("Hello i am non static method");
+    }
+    public static void talbe() {
+
+        for (int i=1;i<=10;i++){
+            System.out.println(i*2);
+            try{
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
+
+public class MethodRefDemo {
+    public static void main(String[] args) {
+
+        // non-static method reference
+        Stuff ob = new Stuff();
+        WorkInter workInter = ob::say;
+        workInter.doTask();
+
+        // static method reference
+        Runnable runnable = Stuff::talbe;
+        Thread th = new Thread(runnable);
+        th.start();
+    }
+}
---
+++ Java_8_Features/MethodRef/WorkInter.java
@@ -0,0 +1,5 @@
+package Java_8_Features.MethodRef;
+
+public interface WorkInter {
+    void doTask();
+}
