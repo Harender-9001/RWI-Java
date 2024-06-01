967648bcf44b3a9dbb664728a56effaa48554b46
Harrybhai1 <harenderrathorepalwal@gmail.com>
2024-06-01 15:03:03 +0530

    Spring project built

---
+++ SpringLearning/src/main/java/com/spring/App.java
@@ -0,0 +1,15 @@
+package com.spring;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        ApplicationContext context = new ClassPathXmlApplicationContext("config.xml");
+        Student student = (Student) context.getBean("student1");
+        System.out.println(student);
+    }
+}
